5720ef70d2b04e7f15b33c5957f0caeec056f7af
Ghostdriver <mattes.hupp@t-online.de>
2023-04-03 10:07:16 +0200

    update systemd files

--- start_discord_bot.sh
+++ start_discord_bot.sh
@@ -1,4 +1,3 @@
-HOME_PATH=/home/ubuntu
-cd $HOME_PATH/Duelyst-Deckroll
+cd ~/Duelyst-Deckroll
 . .venv/bin/activate
-$HOME_PATH/.local/bin/poetry run python3 main.py
+~/.local/bin/poetry run python3 main.py
